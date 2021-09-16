f87b4ae88b76b752603c9f97b5ae491c0c5ab69f
Cristian Brochero <cbrocher@cuc.edu.co>
2021-09-15 22:23:40 -0500

    ejercicio 2

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ if(num_camisas >= 3){
 return `El valor es: ${tot}`;
 } 
 console.log(Ejer1(3,3000));
+
+// 2 ejercicio
+
+let Ejer2 = (valor_com,num_escogido)=>{
+
+    if(num_escogido >= 74){
+            desc = valor_com * 0.20;
+            tot = valor_com - desc;
+
+    }else{  desc = valor_com * 0.15;
+            tot = valor_com - desc;
+         }
+    return `El valor total de la compra con descuento: ${tot}
+        \nDinero descontado: ${desc} `;
+    } 
+    console.log(Ejer2(8000,72));
+    
